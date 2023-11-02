caccc5bd76f6b53fd4b0b0b9a3283f4da5be3d61
devdroide <johne.aplicativos@gmail.com>
2023-11-02 17:07:17 -0500

    crecion de una version con release para prueba chore

--- packages/paquete1/src/paquete1.ts
+++ packages/paquete1/src/paquete1.ts
@@ -3,5 +3,6 @@
 module.exports = paquete1;
 
 function paquete1() {
+  console.info("Ingreso paquete 1")
   return 'Hello from paquete1';
 }
--- packages/paquete2/src/claseImprimir/index.ts
+++ packages/paquete2/src/claseImprimir/index.ts
@@ -1,5 +1,5 @@
 export class Hacer {
   imprimir(texto: any){
-    console.log("Mi texto es:", texto)
+    console.log("Mi texto enviado es:", texto)
   }
 }
