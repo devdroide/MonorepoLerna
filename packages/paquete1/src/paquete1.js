0bdf2469b4ca499e1cd62a2952616d55665d0485
devdroide <johne.aplicativos@gmail.com>
2023-11-07 11:56:02 -0500

    se agrego gulp para borrado y se index en raiz de paquetes [2]

---
+++ packages/paquete1/src/paquete1.js
@@ -0,0 +1,13 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.paquete1ConBeta = exports.paquete1 = void 0;
+function paquete1() {
+    console.info("Ingreso paquete 1");
+    return 'Hola soy el paquete1';
+}
+exports.paquete1 = paquete1;
+function paquete1ConBeta() {
+    console.info("Ingreso paquete 1 Beta");
+    return "Hola soy el paquete1 Beta";
+}
+exports.paquete1ConBeta = paquete1ConBeta;
